7ad87219fb32a26069760a02e82e8c571c30bcb9
Jinva <jinvaxie@gmail.com>
2018-06-26 00:36:31 +0800

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// 页面控制
 Route::get('/', 'PagesController@root')->name('root');
+
+// 用户管理
 Auth::routes();
 
+// 用户注册邮件激活
 Route::group(['middleware' => 'auth'], function() {
+	Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-    // 开始
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+        //
     });
-    // 结束
 });
