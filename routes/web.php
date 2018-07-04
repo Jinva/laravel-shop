5ad514c658b87e1a68947191d7a9d747b9d2b441
Jinva <jinvaxie@gmail.com>
2018-07-04 10:04:07 +0800

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,9 @@ Route::group(['middleware' => 'auth'], function() {
         Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
         // 退款申请
         Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+
+        // 优惠券
+        Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     });
 });
 
